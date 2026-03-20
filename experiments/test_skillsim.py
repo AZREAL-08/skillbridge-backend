f96ef2147c9cbdd5d51a666a0c4d486a9108efc7
Nikhil <nikhilrrvk@gmail.com>
2026-03-20 17:07:08 +0530

    Tests for skillsim

--- experiments/test_skillsim.py
+++ experiments/test_skillsim.py
@@ -1,1 +1,6 @@
 # Sandbox: Run Skill-Sim in isolation
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer('alvperez/skill-sim-model')
+test_embedding = model.encode("React", convert_to_tensor=True)
+print(test_embedding.shape)
